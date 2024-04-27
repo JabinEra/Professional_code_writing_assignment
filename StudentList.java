3faaccd1d8646254cec813c022f2686897302f4d
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-04-27 18:08:40 +0600

    Step 7:Eliminates the ‘done’ control-flow variable. Adds better response for search operation.

--- StudentList.java
+++ StudentList.java
@@ -14,43 +14,66 @@ public class StudentList {
 		String fileContents = readFileContents(Constants.FILE_NAME);
 
 		if (args[0].equals(Constants.ShowAll)) {
-			String words[] = fileContents.split(Constants.StudentEntryDelimiter);
-			for (String word : words) {
-				System.out.println(word);
-			}
+			showAll(fileContents);
 		} else if (args[0].equals(Constants.ShowRandom)) {
-			String words[] = fileContents.split(Constants.StudentEntryDelimiter);
-			int randomNumber = new Random().nextInt(words.length);
-			System.out.println(words[randomNumber]);
+			showRandom(fileContents);
 		} else if (args[0].contains(Constants.AddEntry)) {
-			writeToFile(Constants.FILE_NAME,new SimpleDateFormat(Constants.DATE_FORMAT).format(new Date()),args[0].substring(1));
+			addEntry(Constants.FILE_NAME,args[0].substring(1));
 		} else if (args[0].contains(Constants.FindEntry)) {
-			String words[] = fileContents.split(Constants.StudentEntryDelimiter);
-			boolean done = false;
-			String word = args[0].substring(1);
-			for (int idx = 0; idx < words.length && !done; idx++) {
-				if (words[idx].equals(word)) {
-					System.out.println(Constants.FoundText);
-					done = true;
-				}
-			}
+			findEntry(fileContents,args[0].substring(1));
 		} else if (args[0].contains(Constants.ShowCount)) {
-			char charArray[] = fileContents.toCharArray();
-			boolean in_word = false;
-			int count = 0;
-			for (char character : charArray) {
-				if (character == Constants.Space) {
-					if (!in_word) {
-						count++;
-						in_word = true;
-					} else {
-						in_word = false;
-					}
-				}
-			}
-			System.out.println(count + Constants.WordsFound);
+			showWordCount(fileContents);
 		}
 		System.out.println(Constants.DataLoadedText);
+	}
+	public static void showWordCount(String fileContents) {
+		char charArray[] = fileContents.toCharArray();
+		boolean in_word = false;
+		int count = 0;
+		for (char character : charArray) {
+			if (character == Constants.Space) {
+				if (!in_word) {
+					count++;
+					in_word = true;
+				} else {
+					in_word = false;
+				}
+			}
+		}
+		System.out.println(count + Constants.WordsFound);
+	}
+	public static void findEntry(String fileContents, String word) {
+		String words[] = fileContents.split(Constants.StudentEntryDelimiter);
+		boolean found = false;
+		for (String w : words) {
+			if (w.equals(word)) {
+				System.out.println(Constants.FoundText);
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			System.out.println("Word not found.");
+		}
+	}
+	public static void addEntry(String filename, String word) {
+		writeToFile(filename, new SimpleDateFormat(Constants.DATE_FORMAT).format(new Date()), word);
+	}
+
+	public static void showRandom(String fileContents) {
+		String words[] = fileContents.split(Constants.StudentEntryDelimiter);
+		if (words.length > 0) {
+			int randomNumber = new Random().nextInt(words.length);
+			System.out.println(words[randomNumber]);
+		} else {
+			System.out.println("No words found.");
+		}
+	}
+	public static void showAll(String fileContents) {
+		String words[] = fileContents.split(Constants.StudentEntryDelimiter);
+		for (String word : words) {
+			System.out.println(word);
+		}
 	}
 	public static String readFileContents(String filename) {
 		System.out.println(Constants.LoadingDataText);
