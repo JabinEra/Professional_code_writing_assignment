d04e1b77dcff080d986dbab0d1d8e51d277f66f9
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-04-27 17:53:20 +0600

    Step 5:Replaces string literals with constants, storing those constants in a new class called Constants.java

--- StudentList.java
+++ StudentList.java
@@ -3,71 +3,62 @@ import java.text.*;
 import java.util.*;
 
 public class StudentList {
-    public static void main(String[] args) {
-        // Check arguments
-        if(args == null || args.length != 1){
+	public static void main(String[] args) {
+
+		// Check arguments
+		if(args == null || args.length != 1){
 			System.out.println("Usage: (a | r | c | +WORD | ?WORD)");
 			return; //Exit early.
 		}
 
-        String fileContents = readFileContents("students.txt");
+		String fileContents = readFileContents(Constants.FILE_NAME);
 
-        if (args[0].equals("a")) {
-            System.out.println("Loading data ...");
-            String words[] = fileContents.split(",");
-            for (String word : words) {
-                System.out.println(word);
-            }
-            System.out.println("Data Loaded.");
-        } else if (args[0].equals("r")) {
-            System.out.println("Loading data ...");
-            String words[] = fileContents.split(",");
-            Random randomGenerator = new Random();
-            int randomNumber = randomGenerator.nextInt(words.length);
-            System.out.println(words[randomNumber]);
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("+")) {
-            System.out.println("Loading data ...");
-            String word = args[0].substring(1);
-            Date date = new Date();
-            String dateFormatSample = "dd/mm/yyyy-hh:mm:ss a";
-            DateFormat dateFormat = new SimpleDateFormat(dateFormatSample);
-            String finalDate = dateFormat.format(date);
-            writeToFile("students.txt", ", " + word + "\nList last updated on " + finalDate);
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("?")) {
-            System.out.println("Loading data ...");
-            String words[] = fileContents.split(",");
-            boolean done = false;
-            String word = args[0].substring(1);
-            for (int idx = 0; idx < words.length && !done; idx++) {
-                if (words[idx].equals(word)) {
-                    System.out.println("We found it!");
-                    done = true;
-                }
-            }
-            System.out.println("Data Loaded.");
-        } else if (args[0].contains("c")) {
-            System.out.println("Loading data ...");
-            char charArray[] = fileContents.toCharArray();
-            boolean in_word = false;
-            int count = 0;
-            for (char character : charArray) {
-                if (character == ' ') {
-                    if (!in_word) {
-                        count++;
-                        in_word = true;
-                    } else {
-                        in_word = false;
-                    }
-                }
-            }
-            System.out.println(count + " word(s) found ");
-            System.out.println("Data Loaded.");
-        }
-    }
-
-    public static String readFileContents(String filename) {
+		if (args[0].equals(Constants.ShowAll)) {
+			String words[] = fileContents.split(Constants.StudentEntryDelimiter);
+			for (String word : words) {
+				System.out.println(word);
+			}
+		} else if (args[0].equals(Constants.ShowRandom)) {
+			String words[] = fileContents.split(Constants.StudentEntryDelimiter);
+			Random randomGenerator = new Random();
+			int randomNumber = randomGenerator.nextInt(words.length);
+			System.out.println(words[randomNumber]);
+		} else if (args[0].contains(Constants.AddEntry)) {
+			String word = args[0].substring(1);
+			Date date = new Date();
+			DateFormat dateFormat = new SimpleDateFormat(Constants.DATE_FORMAT);
+			String finalDate = dateFormat.format(date);
+			writeToFile(Constants.FILE_NAME,finalDate,word);
+		} else if (args[0].contains(Constants.FindEntry)) {
+			String words[] = fileContents.split(Constants.StudentEntryDelimiter);
+			boolean done = false;
+			String word = args[0].substring(1);
+			for (int idx = 0; idx < words.length && !done; idx++) {
+				if (words[idx].equals(word)) {
+					System.out.println(Constants.FoundText);
+					done = true;
+				}
+			}
+		} else if (args[0].contains(Constants.ShowCount)) {
+			char charArray[] = fileContents.toCharArray();
+			boolean in_word = false;
+			int count = 0;
+			for (char character : charArray) {
+				if (character == Constants.Space) {
+					if (!in_word) {
+						count++;
+						in_word = true;
+					} else {
+						in_word = false;
+					}
+				}
+			}
+			System.out.println(count + Constants.WordsFound);
+		}
+		System.out.println(Constants.DataLoadedText);
+	}
+	public static String readFileContents(String filename) {
+		System.out.println(Constants.LoadingDataText);
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
 			String fileContents = bufferedReader.readLine();
@@ -78,7 +69,8 @@ public class StudentList {
 		}
 	}
 
-	public static void writeToFile(String filename, String data) {
+	public static void writeToFile(String filename, String finalDate, String word) {
+			String data = ", " + word + "\nList last updated on " + finalDate;
 		try {
 			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filename, true));
 			bufferedWriter.write(data);
